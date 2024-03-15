6c973ed6b998887bac65413b653c2a1833716dd3
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-15 10:40:55 +0700

    membuat step 5

--- AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
+++ AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
@@ -7,7 +7,7 @@ void insertionSort() {
     int i, n, temp, j;
     int arr[11];
     
-    for (i = 1; 1 <= n - 1; 1++) 
+    for (i = 1; 1 <= n - 1; i++) 
      {
         temp = arr[i];
         j = i - 1;
@@ -16,7 +16,7 @@ void insertionSort() {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
-    
+        arr[j + 1] = temp;
     }
 
 }
