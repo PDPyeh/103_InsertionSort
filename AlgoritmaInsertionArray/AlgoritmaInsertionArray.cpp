6fd54591949fda519266663fc1ecd6e589dc4926
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-15 10:48:54 +0700

    membuat void input

--- AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
+++ AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
@@ -3,9 +3,32 @@
 #include <iostream>
 using namespace std;
 
+int i, n, temp, j;
+int arr[11];
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya element pada array = ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "////////////////////////////////" << endl;
+    cout << "/////MASUKAN ELEMEN ARRAY///////" << endl;
+    cout << "////////////////////////////////" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+
 void insertionSort() {
-    int i, n, temp, j;
-    int arr[11];
     
     for (i = 1; 1 <= n - 1; i++) 
      {
@@ -21,8 +44,15 @@ void insertionSort() {
 
 }
 
-int main()
-{
-    
-}
+void display() {
+    cout << endl;
+    cout << "//////////////////////////////////" << endl;
+    cout << " Element Array yang telah tersusun " << endl;
+    cout << "/////////////////////////////////" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+
 
