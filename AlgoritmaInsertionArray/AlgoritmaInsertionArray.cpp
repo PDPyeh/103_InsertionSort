2055dfdfcf74ccc4da43f9d607c26bb3b9097782
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-15 10:37:47 +0700

    membuat step 4 a

--- AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
+++ AlgoritmaInsertionArray/AlgoritmaInsertionArray.cpp
@@ -7,9 +7,14 @@ void insertionSort() {
     int i, n, temp, j;
     int arr[11];
     
-    for (i = 1; 1 <= n - 1; 1++) {
+    for (i = 1; 1 <= n - 1; 1++) 
+     {
         temp = arr[i];
         j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            
+        }
     
     }
 
